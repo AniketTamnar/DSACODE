45082cdd216650b49808ae1eaf7e3806143a847b
AniketTamnar <tamnaraniket8@gmail.com>
2026-09-09 10:42:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.20%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,13 +1,15 @@
-class Solution {
-public:
-    long long countCommas(long long n) {
-        long long ans = 0;
-
-        for (long long p = 1000, commas = 1; p <= n; p *= 1000, commas++) {
-            long long end = min(n, p * 1000 - 1);
-            ans += (end - p + 1) * commas;
+    class Solution {
+    public:
+        long long countCommas(long long n) {
+            long long comma=0;
+            long long base=1000;
+            if(n<1000) return 0;
+            else{
+                while(base<=n){
+                comma+=(n-base+1);
+                base*=1000;}
+            }
+            
+            return comma;
         }
-
-        return ans;
-    }
-};
+    };
