8d0e381abfd363288de04f4649bbb76dcfd927ef
AniketTamnar <tamnaraniket8@gmail.com>
2026-09-17 09:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (79.78%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -5,20 +5,16 @@ public:
         int start = 0;
         int end = nums.size() - 1;
 
-        while (start < end) {
+       while(start<end){
 
-            int mid = start + (end - start) / 2;
+        int mid= (start+end)/2;
 
-            if (nums[mid] > nums[end]) {
-               
-                start = mid + 1;
-            }
-            else {
-               
-                end = mid;
-            }
+        if(nums[mid]>nums[end]){
+            start=mid+1;
+        }else{
+            end=mid;
         }
-
-        return nums[start];
+       }
+       return nums[start];
     }
 };
